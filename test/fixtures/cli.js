c3f10fdd428bb140022c97aa1ab82d78a2096ae9
flowmemo <flowmemo@outlook.com>
2016-07-10 23:09:00 +0800

    add test details, fix win test

--- test/fixtures/cli.js
+++ test/fixtures/cli.js
@@ -1,1 +1,17 @@
-../../cli.js
+#!/usr/bin/env node
+'use strict'
+const fs = require('fs')
+const breakline = require('.')
+
+const argv = require('minimist')(process.argv.slice(2))
+const files = argv._
+files.forEach((file) => {
+  fs.readFile(file, 'utf8', (err, data) => {
+    if (err) throw err
+    const newCode = breakline(data)
+    fs.writeFile(file + '.breakline', newCode, {
+      encoding: 'utf8',
+      flag: 'wx'
+    }, err => { if (err) throw err })
+  })
+})
