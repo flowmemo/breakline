e51c6c0296ca5bacf2f9890e33f7697599604ec9
flowmemo <flowmemo@outlook.com>
2016-07-10 23:07:06 +0800

    add test detail

--- test/test.js
+++ test/test.js
@@ -3,6 +3,7 @@ import test from 'ava'
 const path = require('path')
 const bluebird = require('bluebird')
 const fs = bluebird.promisifyAll(require('fs'))
+const chalk = require('chalk')
 const breakline = require('..')
 
 const fixtures = fs.readdirSync('./fixtures').filter(n => n[0] !== '.')
@@ -10,9 +11,15 @@ test('brealine all files', async t => {
   t.plan(fixtures.length)
   const files = fixtures.map(filename =>
     fs.readFileAsync(path.resolve('./fixtures', filename), 'utf8'))
-  for (let file of files) {
+  for (let i = 0; i < files.length; i++) {
+    let file = files[i]
     let sourceCode = await file
-    breakline(sourceCode)
-    t.pass()
+    try {
+      breakline(sourceCode)
+      console.log(chalk.green(`breakline "${fixtures[i]}" passed`))
+      t.pass()
+    } catch (err) {
+      console.error(chalk.green(`breakline "${fixtures[i]}" failed`))
+    }
   }
 })
